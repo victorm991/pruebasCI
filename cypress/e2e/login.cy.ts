1f0577e5916b18e5897289d2bed536e14ea8beef
victorm991 <vimaes-va@outlook.es>
2024-06-29 10:10:48 -0500

    se agregan nuevos casos de prueba al login, como el envio con los campos vacíos

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -5,9 +5,11 @@ describe("Prueba página login", ()=>{
       datos = data;
     });
   });
-  
-  it('verificación de formulario', ()=>{
+  beforeEach(()=>{
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+  })
+
+  it('verificación de formulario', ()=>{
     cy.location('pathname').should('eq', '/web/index.php/auth/login');
     cy.get('h5.oxd-text--h5').contains('Login');
 
@@ -22,13 +24,29 @@ describe("Prueba página login", ()=>{
     cy.get('button[type="submit"]').contains('Login');
     cy.get('p.oxd-text.oxd-text--p').contains('Forgot your password?');
   });
+  it('Campos vacíos', ()=>{
+    cy.location('pathname').should('eq', '/web/index.php/auth/login');
+    cy.get('button[type="submit"]').contains('Login').click();
+    cy.get('input[name="username"][class="oxd-input--error"]');
+    cy.get('input[name="password"][class="oxd-input--error"]');
+    cy.get('span.oxd-text.oxd-text--span.oxd-input-field-error-message').contains('Required');
+  });
+  /*
+  it('inicio de sesión fallido', ()=>{
+
+  });
+  it('Cambio de contraseña', ()=>{
+
+  });*/
 
   it('Ingreso al software', ()=>{
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
     cy.location('pathname').should('eq', '/web/index.php/auth/login');
 
     cy.get('input[name="username"]').should('be.visible').type(datos.username);
     cy.get('input[name="password"]').should('be.visible').type(datos.password);
     cy.get('.oxd-button').click();
+
+    cy.location('pathname').should('eq','/web/index.php/dashboard/index');
+    cy.get('a.oxd-main-menu-item.active > span').should('be.visible').contains('Dashboard');
   });
 });
