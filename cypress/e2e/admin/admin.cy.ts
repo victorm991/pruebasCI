6fece34dd192287a936f25c137ab6dbe5dea3da5
victorm991 <vimaes-va@outlook.es>
2024-06-29 21:54:26 -0500

    Se crea comando personalizado para el inicio de sesión

---
+++ cypress/e2e/admin/admin.cy.ts
@@ -0,0 +1,21 @@
+describe('Casos de prueba Admin', ()=>{
+    let datos: any;
+    before(()=>{
+        cy.fixture('datos.json').then((data)=>{
+            datos = data;
+        });
+    });
+    beforeEach(()=>{
+        cy.session('sesión', ()=>{
+            cy.visit('https://opensource-demo.orangehrmlive.com');
+            cy.inicioSesion(datos.username, datos.password);
+        });
+    });
+    it('Acceso admin', ()=>{
+        cy.visit('https://opensource-demo.orangehrmlive.com');
+        cy.get('a.oxd-main-menu-item > span').should('be.visible').contains('Admin').click();
+        cy.location('pathname').should('eq', '/web/index.php/admin/viewSystemUsers');
+        cy.get('a.oxd-main-menu-item.active');
+        cy.log('La selección esta activa');
+    });
+});
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,4 +1,4 @@
-/// <reference types="cypress" />
+import  cypress = require("cypress");
 // ***********************************************
 // This example commands.ts shows you how to
 // create various custom commands and overwrite
@@ -8,30 +8,16 @@
 // commands please read more here:
 // https://on.cypress.io/custom-commands
 // ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(email: string, password: string): Chainable<void>
-//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
+declare global {
+    namespace Cypress{
+        interface Chainable {
+            inicioSesion(user: string, pass: string): Chainable <JQuery<Element>>
+        }
+    }
+}
+Cypress.Commands.add<any>('inicioSesion', (user: string, pass: string) =>{
+    cy.location('pathname').should('eq', '/web/index.php/auth/login');
+    cy.get('input[name="username"]').should('be.visible').type(user);
+    cy.get('input[name="password"]').should('be.visible').type(pass);
+    cy.get('.oxd-button').click();
+});
